6fcc0f8bd6af72435eefdb04628aa91a91fa60a1
Timothy Lam <timothylam1228@gmail.com>
2023-11-18 12:19:15 -0500

    update noti

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,9 +18,12 @@ const Home = () => {
       // Check if the browser supports notifications
       alert("This browser does not support desktop notification");
     } else if (Notification.permission === "granted") {
+      console.log('New')
       // Check whether notification permissions have already been granted;
       // if so, create a notification
-      new Notification("Hi there!");
+      new Notification("Example",{
+        body: "This is a notification",
+      })
       // …
     } else if (Notification.permission !== "denied") {
       // We need to ask the user for permission
