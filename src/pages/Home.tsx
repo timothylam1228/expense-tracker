0d6cafbcd0e361caa5a9ca76fbd7f7e43514da0c
Timothy Lam <timothylam1228@gmail.com>
2023-11-18 22:01:40 -0500

    update noti

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,13 +14,17 @@ const Home = () => {
   };
 
   function notifyMe() {
-    if (Notification.permission === "granted") {
-      console.log("New");
+    
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      console.log('New')
       // Check whether notification permissions have already been granted;
       // if so, create a notification
-      new Notification("Example", {
+      new Notification("Example",{
         body: "This is a notification",
-      });
+      })
       // …
     } else if (Notification.permission !== "denied") {
       // We need to ask the user for permission
