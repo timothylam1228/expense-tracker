71d456601afb22b45267711d5234bb9aa55eff97
Timothy Lam <timothylam1228@gmail.com>
2023-11-19 03:19:19 -0500

    remove

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,6 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
-      workbox: {
-        cleanupOutdatedCaches: false,
-      },
       injectRegister: "auto",
       includeAssets: ["icon.png", "icon.svg"],
       manifest: {
